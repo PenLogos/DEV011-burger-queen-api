d00407b0764680ce148b5436048584b6487f98f1
PenLogos <142206877+PenLogos@users.noreply.github.com>
2023-11-30 06:30:31 -0500

    instalación de node driver e intento de conexión con mongo

--- connect.js
+++ connect.js
@@ -1,10 +1,19 @@
+const { MongoClient } = require('mongodb');
 const config = require('./config');
 
 // eslint-disable-next-line no-unused-vars
 const { dbUrl } = config;
+const client = new MongoClient(config.dbUrl);
 
 async function connect() {
-  // TODO: Database Connection
+  try {
+    await client.connect();
+    const db = client.db('burguer-queen');
+    console.log('se eonectó con base de datos');
+    return db;
+  } catch (error) {
+    console.log('erro al conectar con base de datos');
+  }
 }
 
 module.exports = { connect };
